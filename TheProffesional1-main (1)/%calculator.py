ff2e6bd8dd86ed98e9ec5a8dee851390b20ede89
meowmoo1 <102472138+meowmoo1@users.noreply.github.com>
2022-08-14 15:19:08 +0300

    Delete TheProffesional1-main (1) directory

--- TheProffesional1-main (1)/%calculator.py
+++
@@ -1,9 +0,0 @@
-width = input("ENTER WIDTH > ")
-height = input("ENTER HEIGHT > ")
-
-precentage = input("ENTER PRECENTAGE > ")
-
-print("THE WIDTH OF YOUR PHOTO IS : " + str(float(width) / 100 * float(precentage) + float(width)))
-print("THE HEIGHT OF YOUR PHOTO IS :" + str(float(height) / 100 * float(precentage) + float(height)))
-
-input("close? > ")
